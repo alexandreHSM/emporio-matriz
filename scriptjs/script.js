f8bc3f6ebc398c9ec28a3df7c8cfe0692127a201
alexandreHSM <alexandremendonca19@gmail.com>
2025-11-14 14:50:36 -0300

    Add files via upload

--- scriptjs/script.js
+++ scriptjs/script.js
@@ -1,1 +1,26 @@
+const menu = document.querySelector('#menu-icon') //evento click no icone de menu
+const navBar = document.querySelector('.navbar')
 
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('bx-x');
+    navBar.classList.toggle('active');
+})
+
+
+//animação de scroll
+ScrollReveal().reveal('.hero', {
+    origin: 'bottom',
+    duration: 2000,
+    distance: '20%',
+});
+ScrollReveal().reveal('.categorias', {
+    origin: 'bottom',
+    duration: 3000,
+    distance: '20%',
+});
+ScrollReveal().reveal('.produtos', {
+    origin: 'bottom',
+    duration: 3000,
+    distance: '20%',
+});
