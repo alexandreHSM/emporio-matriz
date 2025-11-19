c265399db9fe59ca75c53412d1d8f83ec0a75117
alexandreHSM <alexandremendonca19@gmail.com>
2025-11-19 08:55:21 -0300

    Add files via upload

--- scriptjs/script.js
+++ scriptjs/script.js
@@ -7,3 +7,28 @@ menu.addEventListener('click', () =>{
     navBar.classList.toggle('active');
 })
 
+
+//produtos
+const botoesFiltro = document.querySelectorAll(".filtro");
+const cards = document.querySelectorAll(".produto-card");
+
+botoesFiltro.forEach(btn => {
+    btn.addEventListener("click", () => {
+
+        document.querySelector(".filtro.active").classList.remove("active");
+        btn.classList.add("active");
+
+        const categoria = btn.getAttribute("data-cat");
+
+        cards.forEach(card => {
+            if (categoria === "todos") {
+                card.style.display = "block";
+            } else {
+                card.style.display =
+                    card.getAttribute("data-cat") === categoria
+                        ? "block"
+                        : "none";
+            }
+        });
+    });
+});
